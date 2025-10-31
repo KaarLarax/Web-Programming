3721d11c2590b4b0f73aa9b7c4b80928faeed0de
Carlos Alberto Lara Hernandez <3sslara@gmail.com>
2025-10-30 20:12:37 -0600

    Add new Bootstrap example page with basic layout and responsive design

--- Partial 3/Bootstrap/without bootstrap/script.js
+++
@@ -1,5 +0,0 @@
-const btn = document.getElementById("colorBtn");
-btn.addEventListener("click", () => {
-    document.body.style.backgroundColor =
-        "#" + Math.floor(Math.random() * 16777215).toString(16);
-});
