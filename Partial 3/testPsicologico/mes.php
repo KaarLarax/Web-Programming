7307fa88212ef8f8eb3dd3e2be67aadfdc6fa311
Carlos Alberto Lara Hernandez <3sslara@gmail.com>
2025-10-30 19:40:51 -0600

    Add JavaScript, HTML, CSS, and PHP files for personality test functionality with dynamic content loading

---
+++ Partial 3/testPsicologico/mes.php
@@ -0,0 +1,28 @@
+<?php
+header('Contert-type: text/html; charset=UTF-8');
+if ($_REQUEST['cod'] ==1)
+    echo "<strong>Enero</strong> Abicioso y serio";
+if ($_REQUEST['cod'] == 2)
+    echo "<strog>Febrero</strong> Pensamientos malos";
+if ($_REQUEST["cod"] == 3)
+    echo "malos";
+if ($_REQUEST["cod"] == 4)
+    echo "mal";
+if ($_REQUEST["cod"] == 5)
+    echo "pasa";
+if ($_REQUEST["cod"] == 6)
+    echo "raro";
+if ($_REQUEST["cod"] == 7)
+    echo "adfa";
+if ($_REQUEST["cod"] == 8)
+    echo "afda";
+if ($_REQUEST["cod"] == 9)
+    echo "asdfaccc";
+if ($_REQUEST["cod"] == 10)
+    echo "10";
+if ($_REQUEST["cod"] == 11)
+    echo "11";
+if ($_REQUEST["cod"] == 12)
+    echo "12";
+?>
+Chismosos.
